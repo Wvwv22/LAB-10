437ac23e9164434fcf4ed4a5b7a418a8d422dd55
Wvwv22 <150612888+Wvwv22@users.noreply.github.com>
2023-11-13 16:41:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,24 +2,20 @@ from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
-
 @app.post("/calc/add")
 async def add(x: float, y: float):
     return {"result": x + y}
-
 
 @app.post("/calc/subtract")
 async def subtract(x: float, y: float):
     return {"result": x - y}
 
-
 @app.post("/calc/multiply")
 async def multiply(x: float, y: float):
     return {"result": x * y}
 
-
 @app.post("/calc/divide")
 async def divide(x: float, y: float):
     if y == 0:
-        raise HTTPException(status_code=400, detail="Cannot divide by zero")
+        raise HTTPException(status_code=400, detail="не может делить на ноль")
     return {"result": x / y}
